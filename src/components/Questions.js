7e3ad02dac6338f3f85ce9184d072faf818ba79a
Jana Scheuble <janascheuble@Janas-MacBook-Air.local>
2020-10-27 16:49:53 -0700

    remove question from array after view to eliminate duplicates

--- src/components/Questions.js
+++ src/components/Questions.js
@@ -4,14 +4,18 @@ import data from '../data.json';
 const Questions = () => {
     const [questions, setQuestions] = useState(data)
     const [currentQuestion, setCurrentQuestion] = useState({})
+    const [answers, setAnswers] = useState([])
 
     useEffect(() => {
         const index = Math.floor(Math.random() * Math.floor(questions.length))
         setCurrentQuestion(questions[index])
+        setQuestions(questions.filter((e, i) => {
+            return i !== index;
+        }))
     }, [])
 
     return (
-        <div>
+        <div onClick={() => console.log(questions)}>
             {currentQuestion.question}
         </div>
     )
