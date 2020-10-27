b569e6a4353196dc23e7ae52eb543d4ad679214f
Jana Scheuble <janascheuble@Janas-MacBook-Air.local>
2020-10-27 16:39:05 -0700

    display random question when game starts

--- src/components/Questions.js
+++ src/components/Questions.js
@@ -1,10 +1,18 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
+import data from '../data.json';
 
 const Questions = () => {
+    const [questions, setQuestions] = useState(data)
+    const [currentQuestion, setCurrentQuestion] = useState({})
+
+    useEffect(() => {
+        const index = Math.floor(Math.random() * Math.floor(questions.length))
+        setCurrentQuestion(questions[index])
+    }, [])
 
     return (
         <div>
-            questions
+            {currentQuestion.question}
         </div>
     )
 }
