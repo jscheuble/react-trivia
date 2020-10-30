20fd6ea7c5450566c73b6d92f49a5fbab6a656f6
Jana Scheuble <janascheuble@Janas-MacBook-Air.local>
2020-10-30 14:44:56 -0700

    progress bar styling and colors

--- src/components/ProgressBar.js
+++ src/components/ProgressBar.js
@@ -3,21 +3,22 @@ import styled from 'styled-components';
 
 const Bar = styled.div`
     height: 20px;
-    width: 80%;
-    border: 1px solid #fff;
+    width: 75%;
+    border: 1px solid #80ffdb;
+    box-shadow: 0 0 5px 5px rgba(128, 255, 219, 0.3);
     background: #000;
     border-radius: 50px;
-    margin: 2% auto;
+    margin: 4% auto;
 
     div {
         height: 100%;
-        background: #fff;
+        background: #80ffdb;
         text-align: right;
         border-radius: 50px;
-        padding-right: 10px;
         transition: width 500ms;
-
+        
         span {
+            padding-right: 10px;
             font-size: 1rem;
             color: black;
             text-shadow: 2px 2px #fff;
